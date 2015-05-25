665561d29ee483808c8d6229282cf4843d8270f5
Oleg Efimov <o.efimov@corp.badoo.com>
2015-05-26 00:04:32 +0300

    Add more tests for request checks

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@
  * See license text in LICENSE file
  */
 
-/*global describe, it*/
+/*global describe, it, beforeEach, afterEach */
 
 var assert = require("assert");
 var http = require("http");
@@ -43,54 +43,110 @@ function testJinbaRequestResponse(jinbaRequest, onResponseEnd) {
   request.end();
 }
 
-function assertErrorResponse(requestJson, expectedErrorSubstring, done) {
-  var JS = JinbaServer.createJinbaServer(30002, '127.0.0.1', true);
-  JS.listen(3000, '127.0.0.1');
-
+function assertResponse(requestJson, onResponseJson) {
   testJinbaRequestResponse(requestJson, function (responseText) {
     var responseJson;
     try {
       responseJson = JSON.parse(responseText);
     } catch (e) {
       assert.fail("Cannot parse Jinba server response JSON");
-      JS.close(done);
       return;
     }
 
-    assert.ok(
-      "error" in responseJson,
-      "Response does not contains error description"
-    );
-
-    assert.ok(
-      responseJson.error.indexOf(expectedErrorSubstring) !== -1,
-      "Response error description does not contains substring: " + expectedErrorSubstring
-    );
-
-    JS.close(done);
+    onResponseJson(responseText, responseJson);
   });
 }
 
+function assertResponseGlobalError(requestJson, expectedErrorSubstring) {
+  assertResponse(
+    requestJson,
+    function (responseText, responseJson) {
+      assert.ok(
+        "error" in responseJson,
+        "Response does not contains error description: " + responseText
+      );
+
+      assert.ok(
+        responseJson.error.indexOf(expectedErrorSubstring) !== -1,
+        "Response error description '" + responseJson.error + "' does not contains substring '" + expectedErrorSubstring + "'"
+      );
+    }
+  );
+}
+
+function assertResponseLocalError(requestJson, expectedErrorSubstring) {
+  assertResponse(
+    requestJson,
+    function (responseText, responseJson) {
+      assert.ok(
+        (responseJson instanceof Array) && (responseJson.length == 1),
+        "Response should be an array with one object inside: " + responseText
+      );
+
+      assert.ok(
+        "error" in responseJson[0],
+        "Response does not contains error description: " + responseText
+      );
+
+      assert.ok(
+        responseJson[0].error.indexOf(expectedErrorSubstring) !== -1,
+        "Response error description '" + responseJson[0].error + "' does not contains substring '" + expectedErrorSubstring + "'"
+      );
+    }
+  );
+}
+
 describe('JinbaServer', function () {
+  it('should export createJinbaServer() function', function () {
+    assert.ok(typeof JinbaServer.createJinbaServer === 'function');
+  });
+
   it('should export createRequestListener() function', function () {
     assert.ok(typeof JinbaServer.createRequestListener === 'function');
   });
 
   describe('RequestListener', function () {
-    it('should check for empty POST data', function (done) {
-      assertErrorResponse("", "Empty POST data", done);
+    var JS;
+
+    beforeEach(function () {
+      JS = JinbaServer.createJinbaServer(30002, '127.0.0.1', true);
+      JS.listen(3000, '127.0.0.1');
     });
 
-    it('should check for broken JSON', function (done) {
-      assertErrorResponse("[}", "Cannot parse incoming JSON", done);
+    afterEach(function (done) {
+      JS.close(done);
     });
 
-    it('should check for wring JSON', function (done) {
-      assertErrorResponse("{}", "Incoming JSON is not array", done);
+    it('should check for empty POST data', function () {
+      assertResponseGlobalError("", "Empty POST data");
     });
 
-    it('should check for empty JSON', function (done) {
-      assertErrorResponse("[]", "Incoming JSON is empty", done);
+    it('should check for broken JSON', function () {
+      assertResponseGlobalError("[}", "Cannot parse incoming JSON");
+    });
+
+    it('should check for wring JSON', function () {
+      assertResponseGlobalError("{}", "Incoming JSON is not array");
+    });
+
+    it('should check for empty JSON', function () {
+      assertResponseGlobalError("[]", "Incoming JSON is empty");
+    });
+
+    it('should check for Jinba requests structure', function () {
+      assertResponseLocalError([{}], "request.name is not set");
+    });
+
+    it('should check for Jinba requests timer values', function () {
+      assertResponseLocalError(
+        [
+          {
+            "name": "/",
+            "value": 1000000000
+          }
+        ],
+        "request.value is out of limits"
+      );
     });
   });
 });
