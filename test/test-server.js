d98b7a095313e48e4aad16e42f4a1f05c4bdaf89
Oleg Efimov <o.efimov@corp.badoo.com>
2015-05-25 23:35:52 +0300

    Fix test server

--- test/test-server.js
+++ test/test-server.js
@@ -9,8 +9,6 @@
 
 var JinbaServer = require('../');
 
-return JinbaServer;
-
 console.log("Start listening...");
 JinbaServer.createJinbaServer(30002, '127.0.0.1', true).listen(3000, '127.0.0.1');
 console.log("Listening stared.");
